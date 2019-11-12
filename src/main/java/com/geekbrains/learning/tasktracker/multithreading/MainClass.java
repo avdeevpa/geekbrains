be99107d018b7c12a339973a258a5377966f2237
Pavel Avdeev <pavel.avdeev@domru.ru>
2019-11-12 10:08:08 +0500

    Lesson 07 - debug deleted

--- src/main/java/com/geekbrains/learning/tasktracker/multithreading/MainClass.java
+++ src/main/java/com/geekbrains/learning/tasktracker/multithreading/MainClass.java
@@ -22,7 +22,7 @@ public class MainClass {
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 200 /*+ (int) (Math.random() * 10)*/);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
             cars[i].barrierReady = cbReady;
             cars[i].latchStart = cdRaceStart;
             cars[i].latchEnd = cdRaceEnd;
