a5f25763a0860216bb580c1483cfb195921ac143
Pavel Avdeev <avdeev.p.a@yandex.ru>
2019-12-11 22:13:31 +0500

    Master cleared

--- src/main/java/com/geekbrains/tasktracker/entities/validations/TaskAddEdtGroup.java
+++
@@ -1,4 +0,0 @@
-package com.geekbrains.tasktracker.entities.validations;
-
-public interface TaskAddEdtGroup {
-}
--- src/main/java/com/geekbrains/tasktracker/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.geekbrains.tasktracker.repositories;
-
-import com.geekbrains.tasktracker.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task>, PagingAndSortingRepository<Task, Long> {
-}
